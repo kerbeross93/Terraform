b9529b7428cb1812c37f68571aa924d310aa937b
kerbeross93 <martin930604@gmail.com>
2024-09-18 01:23:41 +0200

    added the policy

--- terraform/labs/Host a S3 Static Website using Terraform/main.tf
+++ terraform/labs/Host a S3 Static Website using Terraform/main.tf
@@ -30,3 +30,35 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_object" "upload_object" {
+  for_each      = fileset("html/", "*")
+  bucket        = aws_s3_bucket.bucket.id
+  key           = each.value
+  source        = "html/${each.value}"
+  etag          = filemd5("html/${each.value}")
+  content_type  = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "read_access_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.bucket.arn}",
+        "${aws_s3_bucket.bucket.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
