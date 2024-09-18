b557df28fdb467358a17b5b5a7a386d285f932b2
kerbeross93 <martin930604@gmail.com>
2024-09-18 14:14:52 +0200

    Security Group within AWS EC2

--- terraform/labs/Launch an EC2 Instance as a web server using Terraform/main.tf
+++ terraform/labs/Launch an EC2 Instance as a web server using Terraform/main.tf
@@ -4,3 +4,19 @@ access_key = "${var.access_key}"
 secret_key = "${var.secret_key}"
 }
 
+resource "aws_security_group" "web-server" {
+  name = "web-server"
+  description = "Allow incoming HTTP Connections" 
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]         
+    }   
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]         
+    }           
+}
