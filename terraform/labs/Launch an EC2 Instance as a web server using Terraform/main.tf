83f801cfc746cb1e02e6003b1bc5636f80adeffd
kerbeross93 <martin930604@gmail.com>
2024-09-18 14:17:05 +0200

    vm added in the code

--- terraform/labs/Launch an EC2 Instance as a web server using Terraform/main.tf
+++ terraform/labs/Launch an EC2 Instance as a web server using Terraform/main.tf
@@ -20,3 +20,22 @@ resource "aws_security_group" "web-server" {
     cidr_blocks = ["0.0.0.0/0"]         
     }           
 }
+
+resource "aws_instance" "web-server" {
+  ami = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+  key_name = "whizlabs-key"
+  security_groups = ["${aws_security_group.web-server.name}"]
+  user_data = <<-EOF
+  #!/bin/bash 
+  sudo su
+  yum update -y
+  yum install httpd -y
+  systemctl start httpd
+  systemctl enable httpd
+  echo "<html><h1> Welcome to Whizlabs. Happy Learning... </h1></html>" >> /var/www/html/index.html       
+  EOF 
+  tags = {
+  Name = "web_instance"           
+  }           
+}
